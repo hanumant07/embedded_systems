f6421961e471dc19b66eb43e4d4759ba5ef551c5
Hanumant Singh <hanumant07@gmail.com>
2025-12-05 16:07:58 -0800

    Fix: Replace <cstddef> with <stddef.h> in my_memmove.h for C compatibility

--- include/my_memmove.h
+++ include/my_memmove.h
@@ -1,18 +1,26 @@
 #ifndef MY_MEMMOVE_H
 #define MY_MEMMOVE_H
 
-#include <cstddef> // for size_t
+#include <stddef.h> // for size_t
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /**
  * @brief Moves n bytes from src to dest.
  * Handles overlapping memory regions.
  * Optimized for word-aligned copies.
- * 
+ *
  * @param dest Destination buffer
  * @param src Source buffer
  * @param n Number of bytes to copy
  * @return void* Pointer to dest
  */
-void* my_memmove(void* dest, const void* src, size_t n);
+void *my_memmove(void *dest, const void *src, size_t n);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // MY_MEMMOVE_H
